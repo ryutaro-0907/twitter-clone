16ef662dfe420264bc367eaf7a42f3f7b29533af
ryutaro-0907 <ryutaro.furutani@gmail.com>
2022-05-20 20:41:04 -0500

    add widgets

---
+++ frontend/components/Widgets.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { SearchIcon } from "@heroicons/react/outline";
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+function Widgets() {
+  return (
+    <div className="col-span-2 mt-5 hidden px-2 lg:inline">
+        {/* Search box */}
+        <div className="flex items-center space-x-2 rounded-full bg-gray-200
+        p-3 ">
+            <SearchIcon className='h-5 w-5 text-gray-400'/>
+            <input type='text' className='flex-1 bg-transparent outline-none' placeholder='Search Twitter'></input>
+        </div>
+
+        <TwitterTimelineEmbed
+         sourceType='profile'
+         screenName='saurabhnemade'
+         options={{height:400}}/>
+    </div>
+  )
+}
+
+export default Widgets
