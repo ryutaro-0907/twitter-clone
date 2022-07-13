4db7fa597c1c265b106dff26bb981a8294b875eb
ryutaro-0907 <ryutaro.furutani@gmail.com>
2022-07-13 15:56:35 -0500

    update readme

--- development.sh
+++ development.sh
@@ -2,4 +2,4 @@ docker-compose build
 docker-compose run --rm --entrypoint "poetry install" backend
 docker-compose run --rm --entrypoint "npm install" frontend
 
-docker-compose up
+# docker-compose up
